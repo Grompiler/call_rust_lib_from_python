43ed8f07fb58e1fd28359c4a221acd5d2b7dc12a
pierre <>
2020-03-14 01:52:49 +0100

    rust lib python bindings

---
+++ src/embed.py
@@ -0,0 +1,8 @@
+from ctypes import cdll
+
+lib = cdll.LoadLibrary("../target/release/libembed.so")
+
+lib.process()
+lib.process_not_pub()
+
+print("done from Python!")
---
+++ src/lib.rs
@@ -0,0 +1,39 @@
+use std::thread;
+
+#[no_mangle]
+pub extern fn process() {
+    let handles: Vec<_> = (0..10).map(|_| {
+        thread::spawn(|| {
+            let mut x = 0;
+            for _ in 0..5000000 {
+                x += 1
+            }
+            x
+        })
+    }).collect();
+
+    for h in handles {
+        println!("Thread finished with count={}",
+                 h.join().map_err(|_| "Could not join a thread!").unwrap());
+    }
+    println!("done from Rust!");
+}
+
+#[no_mangle]
+fn process_not_pub() {
+    let handles: Vec<_> = (0..10).map(|_| {
+        thread::spawn(|| {
+            let mut x = 0;
+            for _ in 0..5000000 {
+                x += 1
+            }
+            x
+        })
+    }).collect();
+
+    for h in handles {
+        println!("Thread finished with count={}",
+                 h.join().map_err(|_| "Could not join a thread!").unwrap());
+    }
+    println!("done from Rust not pub and not extern!");
+}
