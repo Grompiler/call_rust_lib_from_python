8283c49a9bf83d6794b10a7f3c37ec71975fca8e
pierre <@>
2024-01-31 20:18:57 +0100

    [ADD] pyo3

--- src/pitexample.rs
+++ src/pitexample.rs
@@ -1,3 +1,20 @@
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+
+#[pyfunction]
+/// Formats the sum of two numbers as string
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+/// This module is a python module implemented in Rust.
+#[pymodule]
+fn libpit(py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_wrapped(wrap_pyfunction!(sum_as_string))?;
+
+    Ok(())
+}
+
 #[no_mangle]
 pub extern "C" fn print_function() {
     println!("Hello from Rust!");
